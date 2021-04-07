d67a17c824717c09ec3ac0dacb018ec9847ec2ca
Juan Vilada <jvillad1@gmail.com>
2021-04-07 01:34:59 -0500

    feat: Add NASA API buildConfigField -> Read from local.properties (not on version control)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -21,6 +23,13 @@ android {
     }
 
     buildTypes {
+        all {
+            buildConfigField(
+                "String",
+                "NASA_API_KEY",
+                gradleLocalProperties(rootDir).getProperty("nasaApiKey")
+            )
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
@@ -50,13 +59,16 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.activity:activity-compose:1.3.0-alpha05")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
     implementation("com.google.android.material:material:1.3.0")
+
     implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.0")
-    implementation("androidx.activity:activity-compose:1.3.0-alpha03")
-    testImplementation("junit:junit:4.+")
+
+    testImplementation("junit:junit:4.13.2")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 buildscript {
     val compose_version by extra("1.0.0-beta01")
     repositories {
@@ -8,9 +9,6 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.0-alpha13")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle.kts files
     }
 }
 
