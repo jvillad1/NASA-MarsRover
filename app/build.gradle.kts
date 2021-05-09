cc0e91267fe2ec89ed6c54b8ddc96999e430a25a
Juan Vilada <jvillad1@gmail.com>
2021-05-08 23:34:11 -0500

    chore: Fix build for compose 1.0.0-beta06 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,11 +68,11 @@ dependencies {
     // UI
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.activity:activity-compose:1.3.0-alpha06")
+    implementation("androidx.activity:activity-compose:1.3.0-alpha07")
     implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.material:material-icons-core:1.0.0-beta04")
-    implementation("androidx.compose.material:material-icons-extended:1.0.0-beta04")
+    implementation("androidx.compose.material:material-icons-core:1.0.0-beta06")
+    implementation("androidx.compose.material:material-icons-extended:1.0.0-beta06")
     implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
 
     // Lifecycle
@@ -82,12 +82,12 @@ dependencies {
     implementation("androidx.core:core-ktx:1.3.2")
 
     // Dagger Hilt
-    implementation("com.google.dagger:hilt-android:2.33-beta")
-    implementation("androidx.hilt:hilt-common:1.0.0-beta01")
+    implementation("com.google.dagger:hilt-android:2.35.1")
+    implementation("androidx.hilt:hilt-common:1.0.0")
     implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0-alpha01")
-    kapt("com.google.dagger:hilt-compiler:2.33-beta")
-    kapt("androidx.hilt:hilt-compiler:1.0.0-beta01")
+    kapt("com.google.dagger:hilt-compiler:2.35.1")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
 
     // ViewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["compose_version"]}")
@@ -102,18 +102,18 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
     implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.moshi:moshi:1.11.0")
-    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.11.0")
+    implementation("com.squareup.moshi:moshi:1.12.0")
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.12.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
 
     // Room
-    implementation ("androidx.room:room-runtime:2.2.6")
-    implementation ("androidx.room:room-ktx:2.2.6")
-    kapt ("androidx.room:room-compiler:2.2.6")
+    implementation ("androidx.room:room-runtime:2.3.0")
+    implementation ("androidx.room:room-ktx:2.3.0")
+    kapt ("androidx.room:room-compiler:2.3.0")
 
     // Accompanist
-    implementation ("com.google.accompanist:accompanist-glide:0.7.0")
-    implementation ("com.google.accompanist:accompanist-insets:0.7.1")
+    implementation ("com.google.accompanist:accompanist-glide:0.9.1")
+    implementation ("com.google.accompanist:accompanist-insets:0.9.1")
 
     // Test
     testImplementation("junit:junit:4.13.2")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 buildscript {
-    val compose_version by extra("1.0.0-beta01")
+    val compose_version by extra("1.0.0-beta06")
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha14")
+        classpath("com.android.tools.build:gradle:7.0.0-alpha15")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.33-beta")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.35")
     }
 }
 
